b94dc6814606cff901265a4962381d0b94b17f5e
Teedari <godfredderi47@gmail.com>
2022-08-21 08:52:31 +0000

    updated custom tags

--- dashboard/templatetags/custom_tags.py
+++ dashboard/templatetags/custom_tags.py
@@ -12,3 +12,19 @@ def choice_value(value):
 def short_name(value):
   print(value)
   return value.username[0].upper()
+
+@register.filter
+def get_currency(value):
+  return f'GH₵{value}Gp'
+  
+
+
+# @register.inclusion_tag(name='render_image', filename='components/image_component.html')
+# def render_image(**kwargs):
+#   print(kwargs)
+#   if 'src' in kwargs:
+#     return {'src': kwargs['src']}
+#   else: 
+#     return {'src': 'assets/svgs/undraw_empty.svg', 'default': True}
+  
+  
