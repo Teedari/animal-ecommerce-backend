82c2136fa13482a5f51f16d4614cece82c050ee7
Teedari <godfredderi47@gmail.com>
2022-08-24 00:38:15 +0000

    updated

--- core/settings.py
+++ core/settings.py
@@ -210,7 +210,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
