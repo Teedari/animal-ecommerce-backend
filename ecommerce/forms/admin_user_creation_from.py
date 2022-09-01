6d61d938cb0150d9d0fa1afe3c2ca02c4d17f47c
Teedari <godfredderi47@gmail.com>
2022-09-01 01:01:23 +0000

    added the ability for admin to create  agent

--- ecommerce/forms/admin_user_creation_from.py
+++ ecommerce/forms/admin_user_creation_from.py
@@ -1,13 +1,15 @@
 from random import choices
+from unittest import skip
 from django import forms
 from django.contrib.auth.models import User
 from secrets import token_urlsafe
 
 from account.models import UserProfile
+from ecommerce.models import DeliveryPoint
 
 class CustomUserCreationForm(forms.ModelForm):
   user_role = forms.ChoiceField(choices=UserProfile.USER_ROLES, widget=forms.Select(attrs={'class': 'form-control'}))
-
+  delivery_location = forms.MultipleChoiceField(widget=forms.SelectMultiple(attrs={'class': 'form-control'}), required=False)
   
   class Meta:
     model = User
@@ -18,7 +20,12 @@ class CustomUserCreationForm(forms.ModelForm):
       'email': forms.EmailInput(attrs={'class': 'form-control'}),
     }
     
-    
+  def __init__(self, data=None, files=None, *args, **kwargs) -> None:
+    super().__init__(data, files, *args, **kwargs)
+    locations = DeliveryPoint.objects.all()
+    # breakpoint()
+    # self.fields['delivery_location'].={'class': 'form-control'}
+    self.fields['delivery_location'].choices = [(location.id,location.name) for location in locations ]
   
   def save(self, commit=False):
     password = token_urlsafe()
@@ -32,7 +39,16 @@ class CustomUserCreationForm(forms.ModelForm):
     user.save()
     
     try:
-      UserProfile.create_user(user, self.cleaned_data['user_role'])
+      profile = UserProfile.create_user(user, self.cleaned_data['user_role'])
+      # breakpoint()
+      if self.cleaned_data['user_role'] == UserProfile.AGENT:
+        for location in self.cleaned_data.get('delivery_location'):
+          point = DeliveryPoint.objects.filter(id = location)
+          if not point:
+            skip
+          point = point.first()
+          point.userprofile.add(profile)
+          point.save()
     except Exception as err:
       print(err)
   
