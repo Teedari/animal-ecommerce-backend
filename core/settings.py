44ebbe2b362bd033868b7603473e9b1573a01806
Teedari <godfredderi47@gmail.com>
2022-09-03 02:28:28 +0000

    updated token expirery

--- core/settings.py
+++ core/settings.py
@@ -92,8 +92,8 @@ from datetime import timedelta
 ...
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2609),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
