23e03ba2c9332af0fe5ddc3c2bc4ef9ddaaecbc6
Teedari <godfredderi47@gmail.com>
2022-10-12 03:41:38 +0000

    updated build script

--- deployment/build.sh
+++ deployment/build.sh
@@ -5,4 +5,5 @@ source .venv/bin/activate
 python -m pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py makemigrations
 python manage.py migrate
